af45956f36833758c7835671f07bc4b58f624958
H-M-Nizum <hmnizum1714032@gmail.com>
2024-04-20 15:32:07 +0600

    ModelSerializer and it's 3 type of validators

--- DjangoApi/app/serializers.py
+++ DjangoApi/app/serializers.py
@@ -29,7 +29,7 @@ class SchoolSerializers(serializers.Serializer):
         if value > 50:
             raise serializers.ValidationError('Seat must lessthen or equal to 50')
         if value < 20:
-            raise serializers.ValidationError('Seat must less then 20')
+            raise serializers.ValidationError('Seat must Greater then 20')
         return value
 
     # Object Level Validation :def validate(self, data)
@@ -52,12 +52,40 @@ class SchoolSerializers(serializers.Serializer):
     #     teacher_name = serializers.CharField(max_length=50, validators=[starts_with_r])
     #     course_name = serializers.CharField(max_length=50) 
 
+
 # Model serializer
 class StudentSerializers(serializers.ModelSerializer):
+    # validotrs
+    def starts_with_r(value):
+        if value[0].lower() != 'r':
+            raise serializers.ValidationError('Name should start with R')
+
+    student_name = serializers.CharField(validators=[starts_with_r])
     class Meta:
         model = StudentModel
         fields = ['student_name', 'class_name', 'age', 'roll']
         # fields = '__all__'  # Mean all fields in studentModel
+
+    # Field Level Validation : def validate_fieldName(self, value)
+    def validate_age(self, value):
+        if value > 30:
+            raise serializers.ValidationError('Age must less then or equal to 30')
+        if value < 10:
+            raise serializers.ValidationError('Age must Greater then or equal to 10')
+        return value
+    
+    # Object Level Validation :def validate(self, data)
+    def validate(self, data):
+        class_name = data.get('class_name')
+        roll = data.get('roll')
+        c_name = ['six', 'seven','eight', 'nine', 'ten']
+        if class_name.lower() not in c_name:
+            raise serializers.ValidationError('This class are not avabile right now. Avaible six to ten')
+        if roll > 100 or roll < 1:
+            raise serializers.ValidationError('Roll are not allow less then 1 or greater then 100')
+        return data
+    
+
 
 # Model serializer for teacher model
 class TeacherSerializers(serializers.ModelSerializer):
@@ -74,7 +102,10 @@ class FoodCategorySerializer(serializers.ModelSerializer):
         model = FoodCategory
         fields = '__all__'
 
+
 class FoodItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = FoodItem
         fields = '__all__'
+
+        
