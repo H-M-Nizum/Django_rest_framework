8796f8df984a1b374ad2d61568b437346272b6c8
H-M-Nizum <hmnizum1714032@gmail.com>
2024-04-21 21:27:46 +0600

    django session authentication

--- authentication/app/urls.py
+++ authentication/app/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
-from .views import StudentView
+from .views import StudentView, StudentSessionView
 from rest_framework.routers import DefaultRouter
+
 router = DefaultRouter()
 
 router.register('student', StudentView, basename='student')
+router.register('studentsession', StudentSessionView, basename='studentsession')
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+
+    # for login and logout option
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- authentication/app/views.py
+++ authentication/app/views.py
@@ -4,11 +4,11 @@ from .serializers import StudentSerializer
 from rest_framework import viewsets
 
 # for Authentication
-from rest_framework.authentication import BasicAuthentication
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 # for Permission
-from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly
 
-# Create your views here.
+# Create  views for BasicAuthentication.
 class StudentView(viewsets.ModelViewSet):
     queryset = StudentModel.objects.all()
     serializer_class = StudentSerializer
@@ -18,3 +18,16 @@ class StudentView(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     # permission_classes = [AllowAny]
     # permission_classes = [IsAdminUser]
+
+
+# Create views for SessionAuthentication
+class StudentSessionView(viewsets.ModelViewSet):
+    queryset = StudentModel.objects.all()
+    serializer_class = StudentSerializer
+    ## for basic authentication
+    authentication_classes = [SessionAuthentication]
+    ## for authenticated user permission
+    # permission_classes = [IsAuthenticated]
+    # permission_classes = [AllowAny]
+    # permission_classes = [IsAdminUser]
+    permission_classes = [IsAuthenticatedOrReadOnly]
